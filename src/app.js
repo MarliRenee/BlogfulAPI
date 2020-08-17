f5921f95f5f263fa1fe23a88fe1c3bb2addfa69e
Marli Renee <marlirenee@gmail.com>
2020-08-17 16:01:05 -0700

    testing GET /articles

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -14,6 +15,31 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+// We'll need to make a GET /articles endpoint that gets all of the articles from the database.
+//This endpoint can make use of the ArticlesService we created in the checkpoint 14.
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+    //Use the ArticlesService.getAllArticles method inside the endpoint to populate the response.   
+    .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+        // Note we're passing next into the .catch from the promise chain 
+        // so that any errors get handled by our error handler middleware.
+})
+
+//Implement the rest of the endpoint and make the test pass by using 
+//the getById method on the articles service:
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+          res.json(article)
+        })
+        .catch(next)
+})
 
 app.get('/', (req, res) => {
    res.send('Hello, world!')
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
+const knex = require('knex')
 const app = require('./app')
 
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
+// Using app.set('property-name', 'property-value') we can set a property called 'db' 
+// and set the Knex instance as the value.
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
