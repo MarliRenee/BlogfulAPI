f228ba2b28661107aab7369bc3d7857e89f7ea77
Marli Renee <marlirenee@gmail.com>
2020-08-18 15:24:09 -0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -8,52 +8,46 @@ const ArticlesService = require('./articles-service')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
+app.use(helmet())
 
-// We'll need to make a GET /articles endpoint that gets all of the articles from the database.
-//This endpoint can make use of the ArticlesService we created in the checkpoint 14.
 app.get('/articles', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-    //Use the ArticlesService.getAllArticles method inside the endpoint to populate the response.   
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
     .then(articles => {
-            res.json(articles)
-        })
-        .catch(next)
-        // Note we're passing next into the .catch from the promise chain 
-        // so that any errors get handled by our error handler middleware.
+      res.json(articles)
+    })
+    .catch(next)
 })
 
-//Implement the rest of the endpoint and make the test pass by using 
-//the getById method on the articles service:
 app.get('/articles/:article_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
-        .then(article => {
-          res.json(article)
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      if (!article) {
+        return res.status(404).json({
+          error: { message: `Article doesn't exist` }
         })
-        .catch(next)
+      }
+      res.json(article)
+    })
+    .catch(next)
 })
 
 app.get('/', (req, res) => {
-   res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
