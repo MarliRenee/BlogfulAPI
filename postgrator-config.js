13a2cf36d8fd93ca95accc9ad4d69808ab81cd94
Marli Renee <marlirenee@gmail.com>
2020-08-25 13:43:17 -0700

    Upload for Checkpoint 20, prepping for Heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -9,8 +9,8 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DB_URL,
-  //Ternary condition that checks the NODE_ENV and either uses DB_URL or TEST_DB_URL
+
   "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
